87bfc3a444f2e75613520b2cfb756c56eac9584d
alejandropalacios <alejandro.palacios@raisin.com>
2020-08-16 13:29:25 +0200

    Removing unneeded docs part 2

--- techdemo/demo3/project/plugins.sbt
+++
@@ -1,5 +0,0 @@
-resolvers += Classpaths.typesafeReleases
-
-addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "0.14.10")
-
-addSbtPlugin("com.typesafe.sbt" % "sbt-native-packager" % "1.5.1")
--- techdemo/demo4/src/main/scala/record/analyzer/state/GlobalCommands.scala
+++
@@ -1,8 +0,0 @@
-package record.analyzer.state
-
-trait GlobalCommands
-
-case object FetchRecords extends GlobalCommands
-case object FetchRecord extends GlobalCommands
-case object DoneWithJob extends GlobalCommands
-
